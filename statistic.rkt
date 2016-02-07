9616ee865a3c122e3cd8cb7c2b8024d02c3c8924
Alex Reinhart <alex@refsmmat.com>
2016-02-07 16:19:31 -0500

    Add permuted function to return permutation fns

--- statistic.rkt
+++ statistic.rkt
@@ -1,19 +1,17 @@
 #lang racket
 
-; XXX: This module isn't typed because function->statistic is hard to type.
-
-(provide define-statistic)
+(provide define-statistic permuted)
 
 (require math/distributions)
+
+(define (arg->sample arg)
+    (if (distribution? arg)
+        (sample arg)
+        arg))
 
 (define (function->statistic fun)
   (define (pdf query [log? #f])
     (raise exn:fail:unsupported))
-
-  (define (arg->sample arg)
-    (if (distribution? arg)
-        (sample arg)
-        arg))
 
   (define ((sample-from-fun args) [multiple #f])
     (define (sample) (map arg->sample args))
@@ -35,6 +33,29 @@
      (define fun (function->statistic (lambda (args ...) body ...)))]
     [(define-statistic (fun args ... . more) body ...)
      (define fun (function->statistic (lambda (args ... . more) body ...)))]))
+
+;; Take any statistic of two arguments (must be vectors or lists of data) and
+;; return a statistic calculated with the permuted argument data.
+(define (permuted fun)
+  (define (pdf query [log? #f])
+    (raise exn:fail:unsupported))
+
+  (define (shuffle-args xs ys)
+    (define len-x (vector-length xs))
+    (define shuffled (shuffle (append (vector->list xs) (vector->list ys))))
+    (list (list->vector (take shuffled len-x)) (list->vector (drop shuffled len-x))))
+
+  (define ((sample-from-shuffled xs ys) [multiple #f])
+    (define (shuffled-sample) (shuffle-args (arg->sample xs)
+					    (arg->sample ys)))
+    (if multiple
+	(for/list ([i (in-range 0 multiple)]) (apply fun (shuffled-sample)))
+	(apply fun (shuffled-sample))))
+
+  (lambda (xs ys)
+    (if (or (distribution? xs) (distribution? ys))
+	(distribution pdf (sample-from-shuffled xs ys))
+	(fun xs ys))))
 
 (module+ test
   (require rackunit)
